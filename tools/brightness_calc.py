cf2c838f30a5b402506c16fad38d6f7022579477
rodionbukhanevych11 <rodionbukhanevych@gmail.com>
2022-02-01 17:24:37 +0200

    brightness calc update

--- tools/brightness_calc.py
+++ tools/brightness_calc.py
@@ -1,21 +1,14 @@
 import numpy as np
 from scipy.stats import beta
 import math
+import cv2
 
-RED_SENSITIVITY = 0.299
-GREEN_SENSITIVITY = 0.587
-BLUE_SENSITIVITY = 0.114
+def distribution_pmf(dist, start: float, stop: float, nr_of_steps: int):
+    xs = np.linspace(start, stop, nr_of_steps)
+    ys = dist.pdf(xs)
+    return ys / np.sum(ys)
 
-
-def convert_to_brightness_image(image: np.ndarray) -> np.ndarray:
-    if image.dtype == np.uint8:
-        raise ValueError("uint8 is not a good dtype for the image")
-
-    return np.sqrt(
-        image[..., 0] ** 2 * RED_SENSITIVITY
-        + image[..., 1] ** 2 * GREEN_SENSITIVITY
-        + image[..., 2] ** 2 * BLUE_SENSITIVITY
-    )
+RED_PMF = distribution_pmf(beta(2, 2), 0, 1, 256)    
 
 def get_resolution(image: np.ndarray):
     height, width = image.shape[:2]
@@ -23,16 +16,12 @@ def get_resolution(image: np.ndarray):
 
 def brightness_histogram(image: np.ndarray) -> np.ndarray:
     nr_of_pixels = get_resolution(image)
-    brightness_image = convert_to_brightness_image(image)
-    hist, _ = np.histogram(brightness_image, bins=256, range=(0, 255))
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    hsv_brightness = image_hsv[:, :, 2]
+    hist, _ = np.histogram(hsv_brightness, bins=256, range=(0, 255))
+    del image_hsv
     return hist / nr_of_pixels
     
-def distribution_pmf(dist, start: float, stop: float, nr_of_steps: int):
-    xs = np.linspace(start, stop, nr_of_steps)
-    ys = dist.pdf(xs)
-    # divide by the sum to make a probability mass function
-    return ys / np.sum(ys)
-
 def correlation_distance(
     distribution_a: np.ndarray, distribution_b: np.ndarray
 ) -> float:
@@ -42,22 +31,13 @@ def correlation_distance(
     return dot_product / math.sqrt(squared_dist_a * squared_dist_b)
 
 def compute_hdr(img: np.ndarray):
-    img_brightness_pmf = brightness_histogram(np.float32(img))
-    ref_pmf = distribution_pmf(beta(2, 2), 0, 1, 256)
-    return correlation_distance(ref_pmf, img_brightness_pmf)
+    img_brightness_pmf = brightness_histogram(img)
+    return correlation_distance(RED_PMF, img_brightness_pmf)
 
-def pixel_brightness(pixel):
-    r, g, b = pixel
-    return math.sqrt(0.299 * r ** 2 + 0.587 * g ** 2 + 0.114 * b ** 2)
-
-def image_brightness(img):
-    nr_of_pixels = img.shape[0] * img.shape[1]
-    img_sq = img.reshape(img.shape[0]*img.shape[1],3)
-    return sum(pixel_brightness(pixel) for pixel in img_sq) / nr_of_pixels
+def compute_brightness_base(image):
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    hsv_brightness = np.mean(image_hsv[:, :, 2])
+    
+    return hsv_brightness
 
 
-def get_stats(image:np.ndarray):
-    brightness = image_brightness(image)
-    hdr_brightness = compute_hdr(image)
-
-    return hdr_brightness, brightness
